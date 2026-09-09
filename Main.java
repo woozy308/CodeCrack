a22cd38a4efb4972842d3fbb18ad76b204b2ffbe
Jadon Nguyen <jadon1@uw.edu>
2026-09-09 00:50:35 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+// this took me about 40 minutes to make lol ! i still got it //
+// created 9/9/2026 11:58pm -> 12:39am by jadon nguyen //
+// absolutely no AI was used- all brainpower + a tiny bit of searching lol //
+
+public class Main {
+    public static void main(String[] args) {
+
+        Code game = new Code();
+        game.gameIntro();
+        game.playGame();
+
+    }
+}
